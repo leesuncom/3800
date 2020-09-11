9e623b6b16d027e3aefe49b2292c9b9c0d24aeaf
leesuncom <70786826+leesuncom@users.noreply.github.com>
2020-09-11 18:15:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,8 +25,6 @@ rm -rf luci-theme-argon
 #smartdns
 git clone https://github.com/pymumu/smartdns.git
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git
 #
 #svn checkout https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-ssr-python-pro-server
 #svn checkout https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-dark-mod
